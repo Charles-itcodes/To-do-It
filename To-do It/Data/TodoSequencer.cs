6cf17f1b526c65cbc7a0bfb41a63d83129b35d28
Sayeh Ghaderi <sayeh.ghaderi9@gmail.com>
2022-09-22 21:42:42 +0200

    Number 7

---
+++ To-do It/Data/TodoSequencer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace To_do_It.Data
+{
+    internal class TodoSequencer
+    {
+        private static int ToDo;
+        public static int NextToDo()
+        {
+
+            return ToDo++;
+        }
+        public static int Reset()
+        {
+
+            return ToDo = 0;
+
+        }
+    }
+}
